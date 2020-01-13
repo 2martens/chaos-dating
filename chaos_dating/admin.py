4d085cde7538cdc417708bbeae47f2228a6200ca
Denis Smajlović <deni@deni.dk>
2020-01-13 10:34:48 +0100

    Define simple profile model

--- chaos_dating/admin.py
+++ chaos_dating/admin.py
@@ -1,4 +1,25 @@
 # coding=utf-8
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from chaos_dating.models import Profile
+from chaos_dating.models import Gender
+from chaos_dating.models import Interest
+from chaos_dating.models import Wish
 
 # Register your models here.
+admin.site.unregister(User)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    filter_horizontal = ('wishes',)
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ProfileInline,]
+
+admin.site.register(User, UserProfileAdmin)
+
+admin.site.register(Profile)
+admin.site.register(Gender)
+admin.site.register(Interest)
+admin.site.register(Wish)
--- chaos_dating/models.py
+++ chaos_dating/models.py
@@ -1,4 +1,35 @@
 # coding=utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Gender(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Interest(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Wish(models.Model):
+    interest = models.ForeignKey(Interest, models.CASCADE)
+    gender = models.ForeignKey(Gender, models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        unique_together = ["interest", "gender"]
+
+    def __str__(self):
+        return '%s with %ss' % (self.interest, self.gender)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, models.CASCADE)
+
+    gender = models.ForeignKey(Gender, models.SET_NULL, null=True)
+    wishes = models.ManyToManyField(Wish)
+
+    def __str__(self):
+        return self.user.username
