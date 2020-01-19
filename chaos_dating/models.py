a7b6687b8a6f636dc99c89eac907a03d5ca39dfe
Jim Martens <github@2martens.de>
2020-01-19 16:02:32 +0100

    Added signal receivers to save profile upon changes in user object

--- chaos_dating/models.py
+++ chaos_dating/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 class Gender(models.Model):
     name = models.CharField(max_length=16, unique=True)
     
@@ -37,3 +41,14 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
