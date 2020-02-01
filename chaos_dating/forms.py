9cbdf04d10081b5bdbadcc30b4147fd93ad121fd
Jim Martens <github@2martens.de>
2020-02-01 18:09:50 +0100

    Added pronoun to profile form

--- chaos_dating/forms.py
+++ chaos_dating/forms.py
@@ -7,5 +7,5 @@ from chaos_dating.models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['gender', 'wishes']
-        localized_fields = ['gender', 'wishes']
+        fields = ['pronoun', 'gender', 'wishes']
+        localized_fields = ['pronoun', 'gender', 'wishes']
